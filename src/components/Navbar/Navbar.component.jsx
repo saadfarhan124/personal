f85ac6ba391100671ae9e0205a9dc11cc3291c33
Saad Farhan <saadfarhan124@gmail.com>
2021-01-02 17:21:30 +0500

    responsiveness and animation

--- src/components/Navbar/Navbar.component.jsx
+++ src/components/Navbar/Navbar.component.jsx
@@ -2,11 +2,14 @@ import React, {useState, useEffect} from "react";
 import "./Navbar.scss";
 
 import ScrollReveal from 'scrollreveal';
+import M from 'materialize-css'
 
 export const Navbar = () => {
   const [slide, setSlide] = useState(0);
   const [lastScrollY, setLastScrollY] = useState(0);
-
+  const [sidenav, setSideNav] = useState();
+  const [loadedOnce, setLoadedOnce] = useState(false);
+ 
   const handleScroll = () => {
     const currentScrollY = window.scrollY;
     
@@ -48,6 +51,15 @@ export const Navbar = () => {
       // }, timeDelay*(key+1))
      
     }
+    if(!loadedOnce){
+      var elems = document.querySelectorAll('.sidenav');
+      let instance = (M.Sidenav.init(elems, []));
+      console.log(instance);
+      setSideNav(instance);
+      console.log(sidenav);
+      setLoadedOnce(true)
+    }
+    
   });
 
   
@@ -65,7 +77,7 @@ export const Navbar = () => {
             Logo
           </a>
         </div>
-        <a href="#" data-target="mobile-demo" className="sidenav-trigger">
+        <a href="#" data-target="mobile-demo" className="sidenav-trigger" >
           <i className="material-icons">menu</i>
         </a>
         <ul className="right hide-on-med-and-down">
@@ -80,7 +92,7 @@ export const Navbar = () => {
             </a>
           </li>
           <li className="nav-item load-hidden">
-            <a href="sass.html">
+            <a onClick={() => scroll("about")}>
               <code>03.</code> Work
             </a>
           </li>
@@ -100,22 +112,22 @@ export const Navbar = () => {
 
     <ul className="sidenav" id="mobile-demo">
       <li>
-        <a href="sass.html">
+        <a onClick={() => scroll("about")}>
           <code>1.</code> About
         </a>
       </li>
       <li>
-        <a href="sass.html">
+        <a onClick={() => scroll("experience")}>
           <code>2.</code> Experience
         </a>
       </li>
       <li>
-        <a href="sass.html">
+        <a onClick={() => alert('saad')}>
           <code>3.</code> Work
         </a>
       </li>
-      <li>
-        <a href="sass.html">
+      <li onClick={() => scroll("contact")}>
+        <a >
           <code>4.</code> Contact
         </a>
       </li>
@@ -126,124 +138,6 @@ export const Navbar = () => {
   </div>
   )
 }
-// class Navbar extends React.Component {
-//   // state = {
-//   //   slide: 0, // How much should the Navbar slide up or down
-//   //   lastScrollY: 0, // Keep track of current position in state
-//   //   shouldHideNav: true
-//   // };
 
-//   componentWillMount() {
-//     // When this component mounts, begin listening for scroll changes
-//     window.addEventListener("scroll", this.handleScroll);
-//   }
-
-//   componentWillUnmount() {
-//     // If this component is unmounted, stop listening
-//     window.removeEventListener("scroll", this.handleScroll);
-//   }
-
- 
   
 
-//   handleScroll = () => {
-   
-//     const { lastScrollY, shouldHideNav } = this.state;
-//     const currentScrollY = window.scrollY;
-//     if (shouldHideNav) {
-//       if (currentScrollY > lastScrollY) {
-//         this.setState({ slide: "-70px" });
-//       } else {
-//         this.setState({ slide: "0px" });
-//       }
-//       this.setState({ lastScrollY: currentScrollY });
-//     }
-//   };
-
-//   scroll = function(ele) {
-//     let offsetTop = document.getElementById(ele).offsetTop;
-
-//     window.scrollTo({
-//       top: offsetTop - 30,
-//       behavior: "smooth"
-//     });
-//   };
-//   render() {
-//     return (
-//       <div className="navbar-fixed">
-//         <nav
-//           style={{
-//             transition: "0.5s ease",
-//             transform: `translate(0, ${this.state.slide})`
-//           }}
-//         >
-//           <div className="nav-wrapper">
-//             <div className="left">
-//               <a href="#!" className="brand-logo">
-//                 Logo
-//               </a>
-//             </div>
-//             <a href="#" data-target="mobile-demo" className="sidenav-trigger">
-//               <i className="material-icons">menu</i>
-//             </a>
-//             <ul className="right hide-on-med-and-down">
-//               <li>
-//                 <a onClick={() => this.scroll("about")}>
-//                   <code>01.</code> About
-//                 </a>
-//               </li>
-//               <li>
-//                 <a onClick={() => this.scroll("experience")}>
-//                   <code>02.</code> Experience
-//                 </a>
-//               </li>
-//               <li>
-//                 <a href="sass.html">
-//                   <code>03.</code> Work
-//                 </a>
-//               </li>
-//               <li>
-//                 <a onClick={() => this.scroll("contact")}>
-//                   <code>04.</code> Contact
-//                 </a>
-//               </li>
-//               <li>
-//                 <a className="waves-effect waves-light btn btn-resume">
-//                   Resume
-//                 </a>
-//               </li>
-//             </ul>
-//           </div>
-//         </nav>
-
-//         <ul className="sidenav" id="mobile-demo">
-//           <li>
-//             <a href="sass.html">
-//               <code>1.</code> About
-//             </a>
-//           </li>
-//           <li>
-//             <a href="sass.html">
-//               <code>2.</code> Experience
-//             </a>
-//           </li>
-//           <li>
-//             <a href="sass.html">
-//               <code>3.</code> Work
-//             </a>
-//           </li>
-//           <li>
-//             <a href="sass.html">
-//               <code>4.</code> Contact
-//             </a>
-//           </li>
-//           <li>
-//             <a className="waves-effect waves-light btn btn-resume">Resume</a>
-//           </li>
-//         </ul>
-//       </div>
-//     );
-//   }
-// }
-
-// export default Navbar;
