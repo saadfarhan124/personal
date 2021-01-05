f983e94fbd11629aa5ff0b854147669edb48ebd5
Saad Farhan <saadfarhan124@gmail.com>
2021-01-05 20:43:26 +0500

    updated text

--- src/components/Landing/Landing.component.jsx
+++ src/components/Landing/Landing.component.jsx
@@ -21,21 +21,15 @@ export const Landing = () => {
         </div>
         <div className="row">
           <div className="col s12">
-            <h1 className="left-align">I build things for the web.</h1>
+            <h1 className="left-align">I build solutions to your problems.</h1>
           </div>
         </div>
         <div className="row">
-          <div className="col s12">
+          <div className="col s12 l8 m8">
             <div className="about-info">
               <p className="left-align">
-                {/* I'm a software engineer based in Boston, MA specializing in building
-                (and occasionally designing) exceptional, high-quality websites and
-                applications. */}
-                Organized and creative individual with a constant thirst for knowledge and an innate desire for
-                self-improvement that led to the creation of strong foundation of logic, math and programming
-                concepts. Proficient at problem solving having adept time management and communication
-                skills. Consistently seeking to be better computer scientist and aiming to become a detailed
-                oriented data scientist. Recently graduated and currently working as a research assistant at Szabist for the establishment of Predictive Analytics Lab under the Center of Data Analytics and Cloud Computing. 
+                I am an independent software enginneer from Karachi specializing in building 
+                complex back end and occasionally designing the front end. 
               </p>
             </div>
           </div>
