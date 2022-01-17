7e363b16679e95bb3195a2e40f8e32b4a7d13cc1
saadfarhan124 <saadfarhan124@gmail.com>
2022-01-17 18:44:40 +0500

    fixed typo

--- src/components/Landing/Landing.component.jsx
+++ src/components/Landing/Landing.component.jsx
@@ -1,12 +1,11 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 import "./Landing.scss";
-
 
 export const Landing = () => {
   useEffect(() => {
     // alert('saad');
   });
-  return(
+  return (
     <section id="landing" className="container intro-text load-hidden">
       <div className="container">
         <div className="row">
@@ -28,27 +27,24 @@ export const Landing = () => {
           <div className="col s12 l8 m8">
             <div className="about-info">
               <p className="left-align">
-                I am an independent software enginneer from Karachi specializing in building 
-                complex back end and occasionally designing the front end. 
+                I am an independent software engineer from Karachi specializing
+                in building complex back end and occasionally designing the
+                front end.
               </p>
             </div>
           </div>
         </div>
         <div className="row">
           <div className="col s12 btn-get-in-touch-col">
-              <a
+            <a
               className="waves-effect waves-light btn btn-resume btn-large btn-get-in-touch"
               href="mailto:saadfarhan124@gmail.com"
-              >
-                Get in touch
-              </a>      
+            >
+              Get in touch
+            </a>
           </div>
         </div>
       </div>
     </section>
-   
-  )
-}
-  
- 
-
+  );
+};
